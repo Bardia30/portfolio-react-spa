b7de1f136aadd3f621db9b5cd1013ba097244b72
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-07-15 17:44:51 -0400

    added styling for the different states

--- src/components/ProjectsComponents/Project.jsx
+++ src/components/ProjectsComponents/Project.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Projects.scss';
 
 function Project({ image, title, technologies }) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <section className='card'>
-        <img className='card__image' src={image} alt={title} />
-        <h4 className='card__title'>{title}</h4>
-        <section className='card__technologies'>
-            {technologies.map(tech => (
-                <p className='card__tech'>{tech}</p>
-            ))}
-        </section>
+      <div onMouseEnter={() => { setIsHovered(true); }} onMouseLeave={() => { setIsHovered(false); }} className='card__image-div'>
+        <div className={isHovered ? 'card__buttons-visible' : 'card__buttons-invisible'}>
+          <button className='btn card__cta-project'>View Projects</button>
+          <button className='btn card__cta-code'>View Code</button>
+        </div>
+        <img
+          
+          className={isHovered ? "card__image card__image--hover" : 'card__image'}
+          src={image}
+          alt={title}
+        />
+      </div>
+      {/* <img 
+        onMouseEnter={() => { setIsHovered(true);}} 
+        onMouseLeave={() => { setIsHovered(false); }} 
+        className={isHovered ? "card__image card__image--hover" : 'card__image'} 
+        src={image} 
+        alt={title} 
+      /> */}
+      <h4 className='card__title'>{title}</h4>
+      <section className='card__technologies'>
+        {technologies.map(tech => (
+          <p className='card__tech'>{tech}</p>
+        ))}
+      </section>
     </section>
   )
 }
