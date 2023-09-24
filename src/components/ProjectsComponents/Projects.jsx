bd61541f9a6b7b99896c1175053fae30bc7e451c
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-09-24 12:18:07 -0400

    restarting the project

--- src/components/ProjectsComponents/Projects.jsx
+++ src/components/ProjectsComponents/Projects.jsx
@@ -1,11 +1,5 @@
-import React from 'react'
-import { v4 } from 'uuid';
-import projectOne from '../../assets/images/thumbnail-project-1-large.webp';
-import projectTwo from '../../assets/images/thumbnail-project-2-large.webp';
-import projectThree from '../../assets/images/thumbnail-project-3-large.webp';
-import projectFour from '../../assets/images/thumbnail-project-4-large.webp';
-import projectFive from '../../assets/images/thumbnail-project-5-large.webp';
-import projectSix from '../../assets/images/thumbnail-project-6-large.webp';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import Project from './Project';
 
 import './Projects.scss';
@@ -15,67 +9,16 @@ import './Projects.scss';
 // make each card a component
 //technologies p elements enter programmatically
 export default function Projects() {
+    
+    const [projects, setProjects] = useState([]);
 
-    const projects = [
-        {
-            id: v4(),
-            title: "design portfolio",
-            image: projectOne,
-            technologies: [
-                "html",
-                "css"
-            ]
-        },
-        {
-            id: v4(),
-            title: "e-learning landing page",
-            image: projectTwo,
-            technologies: [
-                "html",
-                "css"
-            ]
-        },
-        {
-            id: v4(),
-            title: "todo web app",
-            image: projectThree,
-            technologies: [
-                "html",
-                "css",
-                "javascript"
-            ]
-        }, 
-        {
-            id: v4(),
-            title: "entertainment app",
-            image: projectFour,
-            technologies: [
-                "html",
-                "css",
-                "javascript"
-            ]
-        },
-        {
-            id: v4(),
-            title: "memory game",
-            image: projectFive,
-            technologies: [
-                "html",
-                "css",
-                "javascript"
-            ]
-        },
-        {
-            id: v4(),
-            title: "art gallery showcase",
-            image: projectSix,
-            technologies: [
-                "html",
-                "css",
-                "javascript"
-            ]
-        }
-    ]
+    useEffect(()=> {
+        axios.get('http://localhost:5050')
+            .then(res => {
+                setProjects(res.data)
+            })
+            .catch(err => console.log(err.message));
+    }, [])
 
     return (
         <div className='projects'>
@@ -87,7 +30,7 @@ export default function Projects() {
                 {projects.map(project => (
                     <Project 
                         key={project.id}
-                        image={project.image}
+                        image={`http://localhost:5050/${project.image}`}
                         title={project.title}
                         technologies={project.technologies}
                     />
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,53 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import ring from '../../assets/images/pattern-rings.svg';
 import Skill from './Skill';
 import './Skills.scss';
 
 //hardcoded for now
 function Skills() {
+    const [skillsData, setSkillsData] = useState([]);
 
-    //temporary array of skills
-    const skills = [
-        {skill:'HTML', years: 4},
-        {skill:'CSS', years: 4},
-        {skill:'JavaScript', years: 4},
-        {skill:'Accessibility', years: 4},
-        {skill:'React', years: 3},
-        {skill:'Sass', years: 3},
-    ]
+    useEffect(()=> {
+        axios.get('http://localhost:5050/skills')
+            .then(res => {
+                setSkillsData(res.data);
+            })
+    }, [])
+    
     return (
         <div className='skills__outer-section'>
             <div className='skills'>
-                {skills.map(skill => (
-                    <Skill 
+                {skillsData.map(skill => (
+                    <Skill
+                        key={skill.id} 
                         skill={skill.skill}
                         years={skill.years}
                     />
                 ))}
-                {/* <section className='skills__skill'>
-                    <h3 className='skills__technology'>HTML</h3>
-                    <p className='skills__experience'><span className='skills__years'>4 </span>years of experience</p>
-                </section>
-                <section className='skills__skill'>
-                    <h3 className='skills__technology'>CSS</h3>
-                    <p className='skills__experience'><span className='skills__years'>4 </span>years of experience</p>
-                </section>
-                <section className='skills__skill'>
-                    <h3 className='skills__technology'>JavaScript</h3>
-                    <p className='skills__experience'><span className='skills__years'>4 </span>years of experience</p>
-                </section>
-                <section className='skills__skill'>
-                    <h3 className='skills__technology'>Accessibility</h3>
-                    <p className='skills__experience'><span className='skills__years'>4 </span>years of experience</p>
-                </section>
-                <section className='skills__skill'>
-                    <h3 className='skills__technology'>React</h3>
-                    <p className='skills__experience'><span className='skills__years'>3 </span>years of experience</p>
-                </section>
-                <section className='skills__skill'>
-                    <h3 className='skills__technology'>Sass</h3>
-                    <p className='skills__experience'><span className='skills__years'>3 </span>years of experience</p>
-                </section> */}
             </div>
             <img className='skills__rings' src={ring} alt="rings" />
         </div>
