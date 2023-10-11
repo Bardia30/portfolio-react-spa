f4877fb30e6f899a06fb22d136b0722686f11a5d
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-10-10 22:39:43 -0400

    personalized homepage

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import circle from '../../assets/images/pattern-circle.svg'
-import profilePic from '../../assets/images/image-profile-desktop.webp';
-import profilePicMobile from '../../assets/images/image-profile-mobile.webp';
+import profilePic from '../../assets/images/hero-image.jpeg';
 import rings from '../../assets/images/pattern-rings.svg';
 import './Hero.scss';
 
@@ -35,13 +34,13 @@ function Hero() {
         {isMobile ?
           <div className='hero__upper-section'>
             <img className='hero__circle-mobile' src={circle} alt="circle-pattern" />
-            <img className='hero__profile-mobile' src={profilePicMobile} alt="profile" />
+            <img className='hero__profile-mobile' src={profilePic} alt="profile" />
         </div> : null
         }
         <section className='hero__left-section'>
-            <h1 className='hero__title'>Nice to <br className='hero__br'/> meet you! I'm <br className='hero__br'/> Adam Keyes.</h1>
+            <h1 className='hero__title'>Nice to <br className='hero__br'/> meet you! I'm <br className='hero__br'/> Bardia Dehbasti.</h1>
             <img className='hero__rings' src={rings} alt="rings" />
-            <p className="hero__description">Based in the UK, I'm a front-end developer passionate about building accessible web apps that users love.</p>
+            <p className="hero__description">Software Engineer, specialized in React.JS, Next.JS, Node.JS, Express.JS and MongoDB, based in Toronto, ON.</p>
             <button className='hero__cta btn'>contact me</button>
         </section>
         {isMobile ? null: 
